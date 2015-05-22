de65acda23dc4ce455f61fc62866cc48c3c91008
Mathew Peterson <mathewpeterson@me.com>
2015-05-22 12:15:51 -0500

    Fixes php-cs issues

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,5 +8,4 @@
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
  */
-
-require_once(__DIR__ . '/autoload.php');
+require_once __DIR__.'/autoload.php';
